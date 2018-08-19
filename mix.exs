2af0527de6d6ee98a6f669d91e3a54a63061b2b0
Tommy Chen <tommy351@gmail.com>
2018-08-19 18:08:50 +0800

    Update mix.exs
    - Remove application

--- mix.exs
+++ mix.exs
@@ -16,12 +16,10 @@ defmodule PlugProxy.Mixfile do
       lockfile: lockfile(),
       test_coverage: [tool: ExCoveralls],
       preferred_cli_env: preferred_cli_env(),
-      docs: [main: "PlugProxy", source_ref: @version, source_url: @github_link]
+      source_url: @github_link,
+      homepage_url: @github_link,
+      docs: [main: "PlugProxy"]
     ]
-  end
-
-  def application do
-    [applications: [:logger, :cowboy, :plug, :hackney]]
   end
 
   defp elixirc_paths(:test), do: ["lib", "test/support"]
@@ -40,13 +38,17 @@ defmodule PlugProxy.Mixfile do
       {:plug, "~> 1.5"},
       {:hackney, "~> 1.10"},
       {:ex_doc, "~> 0.19", only: :dev, runtime: false},
-      {:excoveralls, "~> 0.7", only: :test},
-      {:inch_ex, "~> 0.5", only: [:dev, :test]}
+      {:excoveralls, "~> 0.7", only: :test, runtime: false},
+      {:inch_ex, "~> 0.5", only: [:dev, :test], runtime: false}
     ]
   end
 
   defp package do
-    [maintainers: ["Tommy Chen"], licenses: ["MIT License"], links: %{"GitHub" => @github_link}]
+    [
+      maintainers: ["Tommy Chen"],
+      licenses: ["MIT License"],
+      links: %{"GitHub" => @github_link}
+    ]
   end
 
   defp preferred_cli_env do
